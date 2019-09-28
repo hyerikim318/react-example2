b9cb2e8a35d97d663197c6e9c3132a11bae5dd83
hyeri <lilyantina@naver.com>
2019-09-28 14:07:56 +0900

    init commit

---
+++ src/InputTest.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+/*
+*   function:  함수형 => 한개의 기능
+*   <InputTest name="aaa"/>
+*   function InputTest() {
+*       return();
+*   }
+*   class: 클래스 => 여러개 기능을 합쳐서 처리
+*/
+
+class InputTest extends Component {
+
+}
+
+export default inputTest;
